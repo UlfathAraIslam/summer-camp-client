fa96bcb9f0f4f70c498832c006b1c60bc78d2aec
Ulfath Ara Islam <mouliajsahid@gmail.com>
2023-06-15 23:31:51 +0900

    user can select course after login

--- src/Pages/Classes/Classes.jsx
+++ src/Pages/Classes/Classes.jsx
@@ -5,28 +5,36 @@ import { Link } from 'react-router-dom';
 
 const Classes = () => {
     const [classes] = useClasses();
-    const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
+
+    const handleSelectButtonClick = () => {
+        if (!user){
+            alert('please log in to select the course')
+        }
+    }
     return (
         <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+
             {
                 classes.map((classItem, _id) => (
                     <div key={_id} className="card bg-base-100 shadow-xl">
-                <figure className="px-10 pt-10">
-                    <img src={classItem.classImage}className="rounded-xl h-[400px]" />
-                </figure>
-                <div className="card-body items-center text-center">
-                    <h2 className="card-title">{classItem.className}</h2>
-                    <p><span className='text-success'>Instructor:</span> {classItem.instructorName}</p>
-                    <p>{classItem.availableSeats}</p>
-                    <p>${classItem.price}</p>
-                    <Link><button className="btn btn-outline font-bold btn-success">Select</button></Link>
-                </div>
-               
+                        <figure className="px-10 pt-10">
+                            <img src={classItem.classImage} className="rounded-xl h-[400px]" />
+                        </figure>
+                        <div className="card-body items-center text-center">
+                            <h2 className="card-title">{classItem.className}</h2>
+                            <p><span className='text-success'>Instructor:</span> {classItem.instructorName}</p>
+                            <p>{classItem.availableSeats}</p>
+                            <p>${classItem.price}</p>
 
-            </div>
+                            <Link><button className="btn btn-outline font-bold btn-success" onClick={handleSelectButtonClick}>Select</button></Link>
+                        </div>
+
+
+                    </div>
                 ))
             }
-            
+
         </section>
     );
 };
